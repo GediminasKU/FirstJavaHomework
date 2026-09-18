999bfd82d275b800a45fc7c0ef37c37b96017589
GediminasGricius <88775405+GediminasGricius@users.noreply.github.com>
2026-09-18 17:58:56 +0300

    Homework

--- src/main/java/lt/vc/Main.java
+++ src/main/java/lt/vc/Main.java
@@ -1,9 +1,29 @@
-package lt.vcd;
+package lt.vc;
 
+
+import java.util.Scanner;
 
 public class Main {
     static void main() {
-        System.out.println("Hello, this is JAVA");
+        Scanner in=new Scanner(System.in);
+
+
+        System.out.println("Hello ");
+        System.out.print("Please enter x value: ");
+        int x=in.nextInt();
+
+        System.out.print("Please enter y value: ");
+        int y=in.nextInt();
+
+        System.out.println(   x / y  );
+
+
 
     }
+
+
+
+
+
+
 }
